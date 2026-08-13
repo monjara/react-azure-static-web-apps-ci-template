ab8dd3d1988f8d16bdf30ec62077a73250bb0ee6
monjara <yaginumtt@gmail.com>
2026-08-13 12:02:06 +0900

    remove unnecessary descriptions

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,5 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
 import '../styles.css'
 
 export const Route = createRootRoute({
@@ -11,19 +8,6 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <>
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'TanStack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
+    <Outlet />
   )
 }
